70fd8785a5d52e6e0deef0a1c06df2f013579bd6
Danyelss <mos.dani@yahoo.com>
2021-12-05 03:04:09 +0200

    Completed

--- lib/shared/menu_drawer.dart
+++ lib/shared/menu_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/session_screen.dart';
 import '../screens/weather_screen.dart';
 import '../screens/bmi_screen.dart';
 import '../screens/intro_screen.dart';
@@ -43,6 +44,9 @@ class MenuDrawer extends StatelessWidget {
               break;
             case 'Weather':
               screen = WeatherScreen();
+              break;
+            case 'Training':
+              screen = SessionsScreen();
               break;
             default:
           }
